1da23da5070471fab579cd4f924bb0963cc130ee
tocode <andrey.zarev@outlook.com>
2024-03-17 02:30:39 +0200

    Update navbar.module.ts

--- exam-project/src/app/navbar/navbar.module.ts
+++ exam-project/src/app/navbar/navbar.module.ts
@@ -1,16 +1,52 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar.component';
+import { NavbarComponent } from './navbarComppnent/navbar.component';
 import { RouterLink } from '@angular/router';
 
+import { HomeComponent } from '../views/home/home.component'; '';
+import { RegisterComponent } from '../views/register/register.component';
+import { LoginComponent } from '../views/login/login.component';
+import { LogoutComponent } from '../views/logout/logout.component';
+import { CreateOfferComponent } from '../views/create-offer/create-offer.component';
+import { OffersComponent } from '../views/offers/offers.component';
+import { ErrorPageComponent } from '../views/error-page/error-page.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'create-offer', component: CreateOfferComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: 'logout', component: LogoutComponent },
+
+  { path: 'offers', component: OffersComponent },
+
+  { path: '**', component: ErrorPageComponent },
+];
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    CreateOfferComponent,
+    OffersComponent,
+    ErrorPageComponent,
   ],
   imports: [
     CommonModule,
-    RouterLink
+    RouterLink,
+
+
+
+    RouterModule.forChild(routes),
+
   ],
   exports: [NavbarComponent]
 })
