484ac9a56213471ee75cfc8ce181f8c94238516c
tocode <andrey.zarev@outlook.com>
2024-03-23 00:27:18 +0200

    Update logout.component.ts

--- exam-project/src/app/views/logout/logout.component.ts
+++ exam-project/src/app/views/logout/logout.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { UserService } from 'src/app/user.service';
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
 export class LogoutComponent {
+  constructor(private userService: UserService) { }
 
+  logout() {
+    debugger
+    this.userService.logout
+
+
+
+  }
 }
