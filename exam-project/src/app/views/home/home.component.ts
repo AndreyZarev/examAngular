0ba4b960dd262004f5a898cfd5fafcfae3766484
tocode <andrey.zarev@outlook.com>
2024-03-22 17:14:14 +0200

    Update home.component.ts

--- exam-project/src/app/views/home/home.component.ts
+++ exam-project/src/app/views/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service-api/service-api.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  constructor(private api: ApiService) { }
+  themes: any = []
+  ngOnInit(): void {
+    this.api.getCatalog().subscribe((themes) => {
+      console.log(themes);
+      this.themes = themes
+    })
+  }
 }
