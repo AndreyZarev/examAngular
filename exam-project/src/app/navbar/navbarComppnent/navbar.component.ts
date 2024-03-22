372d2d8afe307a4ef8c46bedc3df48fb8352f7f7
tocode <andrey.zarev@outlook.com>
2024-03-23 00:27:05 +0200

    Update navbar.component.ts

--- exam-project/src/app/navbar/navbarComppnent/navbar.component.ts
+++ exam-project/src/app/navbar/navbarComppnent/navbar.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { UserService } from 'src/app/user.service';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  constructor(private router: Router) { }
-
-  navicationTo(path: string) {
-    this.router.navigate([path]);
+  router: any;
+  constructor(private UserService: UserService, router: RouterModule) { }
+  logout(): void {
+    this.UserService.logout();
+    this.router.navigate(['/home']), { refeerence: this.router.navigation };
   }
+  get isLoggedIn(): boolean {
+
+
+
+    return this.UserService.isLogged
+
+
+  }
+
+
+
 }
