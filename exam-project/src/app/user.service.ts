91b4743cf29840f3fcdd00993d6cebdef360f1c0
tocode <andrey.zarev@outlook.com>
2024-03-23 00:27:07 +0200

    Update user.service.ts

--- exam-project/src/app/user.service.ts
+++ exam-project/src/app/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { User } from "src/interface/offer";
+import { User } from "src/interface/user";
 
 @Injectable({
     providedIn: 'root',
@@ -7,13 +7,34 @@ import { User } from "src/interface/offer";
 
 export class UserService {
     user: User | undefined
-    isLoggedIn: boolean = false
     USER_KEY = '[user]'
-    constructor() {
-        const lsUser = localStorage.getItem(this.USER_KEY) || "";
-        this.user = JSON.parse(lsUser)
-    }
-    login() { }
 
-    logout() { }
+    get isLogged(): boolean {
+        return !!this.user;
+    }
+    constructor() {
+        try {
+            const lsUser = localStorage.getItem(this.USER_KEY) || "";
+            this.user = JSON.parse(lsUser)
+        } catch (err) {
+            console.log(err);
+
+        }
+    }
+    login() {
+        debugger
+        this.user = {
+            firstName: "Pesho",
+            email: "pesho@gmail.com",
+            password: "pass1",
+            rePassword: "pass1",
+        }
+        localStorage.setItem(this.USER_KEY, JSON.stringify(this.user))
+    }
+
+    logout() {
+
+        this.user = undefined;
+        localStorage.removeItem(this.USER_KEY)
+    }
 }
