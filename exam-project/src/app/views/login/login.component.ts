cb50538a16769c019f4cf35b3617ea3a32d61720
tocode <andrey.zarev@outlook.com>
2024-03-24 00:48:13 +0200

    Update login.component.ts

--- exam-project/src/app/views/login/login.component.ts
+++ exam-project/src/app/views/login/login.component.ts
@@ -1,16 +1,24 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/user.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private userService: UserService) { }
-  login(ev: Event, email: string, password: string) {
+
+  constructor(private userService: UserService, private router: Router) { }
+  login(ev: Event, email: string, password: string): void {
     ev.preventDefault()
-    this.userService.login()
-    console.log(email);
+    try {
+      this.userService.login()
+      this.router.navigate(['/home'])
+      debugger
+    } catch (err) {
+      console.log(err);
+    }
 
   }
 }
