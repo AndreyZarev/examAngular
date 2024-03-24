53691d4cd505a04560f75fa7a0c234e7fc689057
tocode <andrey.zarev@outlook.com>
2024-03-25 00:02:05 +0200

    Update create-offer.component.ts

--- exam-project/src/app/views/create-offer/create-offer.component.ts
+++ exam-project/src/app/views/create-offer/create-offer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../service-api/service-api.service';
 
 @Component({
   selector: 'app-create-offer',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-offer.component.css']
 })
 export class CreateOfferComponent {
+  constructor(private apiService: ApiService) { }
+  createOffer(names: string, phone: string, hand: string, weight: string, place: string, bet: number | string) {
+    debugger
+    // ev.preventDefault();
+    console.log(names, phone, hand, weight, place, bet);
+    // try {
 
+
+    //   this.apiService.newOffer(names, phone, hand, weight, place, Number(bet)).subscribe((offer) => {
+    //     console.log(offer);
+    //   })
+
+    // } catch (err) {
+    //   console.log(err);
+
+    // }
+  }
 }
