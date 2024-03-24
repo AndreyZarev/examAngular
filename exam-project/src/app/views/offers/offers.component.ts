749d00a2b1bf78016f4fb3fd43168c76c4db9251
tocode <andrey.zarev@outlook.com>
2024-03-25 00:02:21 +0200

    Update offers.component.ts

--- exam-project/src/app/views/offers/offers.component.ts
+++ exam-project/src/app/views/offers/offers.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service-api/service-api.service';
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
   styleUrls: ['./offers.component.css']
 })
-export class OffersComponent {
+export class OffersComponent implements OnInit {
+  catalog: any = {}
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.apiService.getCatalog().subscribe((data) => {
+      console.log(data);
+      this.catalog = data
+    })
+  }
+
+
 
 }
