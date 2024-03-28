b526b2cf47ea7f512c99798aa6127f2e135fbd9d
tocode <andrey.zarev@outlook.com>
2024-03-28 12:22:56 +0200

    Update login.component.ts

--- exam-project/src/app/views/login/login.component.ts
+++ exam-project/src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from 'src/app/user.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,8 +11,8 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 
   constructor(private userService: UserService, private router: Router) { }
-  login(ev: Event, email: string, password: string): void {
-    ev.preventDefault()
+  login(form: NgForm): void {
+
     try {
       this.userService.login()
       this.router.navigate(['/home'])
