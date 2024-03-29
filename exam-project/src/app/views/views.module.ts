9b11c22dbdfe9e67583c3713615fb4dc0e7f47e1
tocode <andrey.zarev@outlook.com>
2024-03-30 00:59:56 +0200

    Update views.module.ts

--- exam-project/src/app/views/views.module.ts
+++ exam-project/src/app/views/views.module.ts
@@ -1,47 +1,47 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
 
-import { RouterLink } from '@angular/router';
-import { NavbarModule } from '../navbar/navbar.module';
-// import { HomeComponent } from '../views/home/home.component'; '';
-// import { RegisterComponent } from '../views/register/register.component';
-// import { LoginComponent } from '../views/login/login.component';
-// import { LogoutComponent } from '../views/logout/logout.component';
-// import { CreateOfferComponent } from '../views/create-offer/create-offer.component';
-// import { OffersComponent } from '../views/offers/offers.component';
-// import { ErrorPageComponent } from '../views/error-page/error-page.component';
+// import { RouterLink } from '@angular/router';
+// import { NavbarModule } from '../navbar/navbar.module';
+// // import { HomeComponent } from '../views/home/home.component'; '';
+// // import { RegisterComponent } from '../views/register/register.component';
+// // import { LoginComponent } from '../views/login/login.component';
+// // import { LogoutComponent } from '../views/logout/logout.component';
+// // import { CreateOfferComponent } from '../views/create-offer/create-offer.component';
+// // import { OffersComponent } from '../views/offers/offers.component';
+// // import { ErrorPageComponent } from '../views/error-page/error-page.component';
 
-import { RouterModule, Routes } from '@angular/router';
+// import { RouterModule, Routes } from '@angular/router';
 
-// const routes: Routes = [
+// // const routes: Routes = [
 
-//     { path: 'home', component: HomeComponent },
+// //     { path: 'home', component: HomeComponent },
 
-//     { path: 'register', component: RegisterComponent },
-//     { path: 'create-offer', component: CreateOfferComponent },
-//     { path: 'login', component: LoginComponent },
+// //     { path: 'register', component: RegisterComponent },
+// //     { path: 'create-offer', component: CreateOfferComponent },
+// //     { path: 'login', component: LoginComponent },
 
-//     { path: 'logout', component: LogoutComponent },
+// //     { path: 'logout', component: LogoutComponent },
 
-//     { path: 'offers', component: OffersComponent },
+// //     { path: 'offers', component: OffersComponent },
 
-//     { path: '**', component: ErrorPageComponent },
-// ];
+// //     { path: '**', component: ErrorPageComponent },
+// // ];
 
-@NgModule({
-    declarations: [
-
-
-    ],
-    imports: [
-        CommonModule,
-        RouterLink,
-        NavbarModule,
+// @NgModule({
+//     declarations: [
 
 
+//     ],
+//     imports: [
+//         CommonModule,
+//         RouterLink,
+//         NavbarModule,
 
 
-    ],
-    exports: [NavbarModule]
-})
-export class ViewsModel { }
+
+
+//     ],
+//     exports: [NavbarModule]
+// })
+// export class ViewsModel { }
