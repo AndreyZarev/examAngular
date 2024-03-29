c2692368ff5c3f667b177757dabfd5997f64cc46
tocode <andrey.zarev@outlook.com>
2024-03-30 00:33:17 +0200

    Update validators.directive.ts

--- exam-project/src/app/shared/validators/validators.directive.ts
+++ exam-project/src/app/shared/validators/validators.directive.ts
@@ -10,6 +10,7 @@ import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@an
   }]
 })
 export class ValidatorsDirective implements Validator {
+  reGex = "/\w+@\w{2,}\.(bg|com)/gm"
   validate(control: AbstractControl<any, any>): ValidationErrors | null {
     return null
   }
