246a34b3c8c2ad855646eea88640eb079902b47b
tocode <andrey.zarev@outlook.com>
2024-03-31 01:00:27 +0200

    Update email-validator.ts

--- exam-project/src/app/shared/utils/email-validator.ts
+++ exam-project/src/app/shared/utils/email-validator.ts
@@ -3,7 +3,7 @@ import { ValidatorFn } from "@angular/forms";
 export function validateEmail(domains: string[]): ValidatorFn | null {
     return (control) => {
         console.log("Control value: " + control.value);
-
+        const reGex = new RegExp("/\w+@\w{2,}\.(bg|com)/gm")
         return null
 
     }
