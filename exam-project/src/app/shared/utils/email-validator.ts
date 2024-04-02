f2f03de126c086a9c9af877b3aede6a816ec89d0
tocode <andrey.zarev@outlook.com>
2024-04-03 02:05:11 +0300

    Update email-validator.ts

--- exam-project/src/app/shared/utils/email-validator.ts
+++ exam-project/src/app/shared/utils/email-validator.ts
@@ -23,7 +23,7 @@ import { ValidatorFn } from '@angular/forms';
 export function validateEmail(domains: string[]): ValidatorFn {
     const domainString = domains.join('|');
     const regExp = new RegExp(`[A-Za-z0-9]+@gmail\.(${domainString})`);
-
+    debugger
     return (control) => {
         const isEmailInvalid = control.value === '' || regExp.test(control.value);
         return isEmailInvalid ? null : { validateEmail: true };
