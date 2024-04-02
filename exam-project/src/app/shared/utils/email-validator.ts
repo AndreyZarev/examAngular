f3878b051bdc39b803490f25690915bdc834d3c4
tocode <andrey.zarev@outlook.com>
2024-04-03 02:17:01 +0300

    Update email-validator.ts

--- exam-project/src/app/shared/utils/email-validator.ts
+++ exam-project/src/app/shared/utils/email-validator.ts
@@ -1,21 +1,3 @@
-// import { ValidatorFn } from "@angular/forms";
-
-// export function validateEmail(domains: string[]): ValidatorFn {
-//     const domainStrings = domains.join("|")
-//     const reGex = new RegExp(`\w*.?\w*@\w{2,}\.(${domainStrings})`)
-
-//     //still returns false
-
-//     return (control) => {
-//         const isInvalid = control.value === "" || reGex.test(control.value);
-//         debugger
-//         console.log("Test Regex: ", isInvalid, "control value", control.value);
-
-//         return isInvalid ? null : { validateEmail: true }
-
-//     }
-// }
-
 
 
 import { ValidatorFn } from '@angular/forms';
