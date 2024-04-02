370bf7b2d6d9c212b05a5eac46994ce5881d3239
tocode <andrey.zarev@outlook.com>
2024-04-03 02:30:16 +0300

    Update email-validator.ts

--- exam-project/src/app/shared/utils/email-validator.ts
+++ exam-project/src/app/shared/utils/email-validator.ts
@@ -1,10 +1,29 @@
+// import { ValidatorFn } from "@angular/forms";
+
+// export function validateEmail(domains: string[]): ValidatorFn {
+//     const domainStrings = domains.join("|")
+//     const reGex = new RegExp(`\w*.?\w*@\w{2,}\.(${domainStrings})`)
+
+//     //still returns false
+
+//     return (control) => {
+//         const isInvalid = control.value === "" || reGex.test(control.value);
+//         debugger
+//         console.log("Test Regex: ", isInvalid, "control value", control.value);
+
+//         return isInvalid ? null : { validateEmail: true }
+
+//     }
+// }
+
 
 
 import { ValidatorFn } from '@angular/forms';
 
 export function validateEmail(domains: string[]): ValidatorFn {
     const domainString = domains.join('|');
-    const regExp = new RegExp(`[A-Za-z0-9]+@gmail\.(${domainString})`);
+    const regExp = new RegExp(`\w*.?\w*@\w{2,}\.(${domainString})`)
+    // const regExp = new RegExp(`[A-Za-z0-9]+@gmail\.(${domainString})`);
     debugger
     return (control) => {
         const isEmailInvalid = control.value === '' || regExp.test(control.value);
