7e49d557682e44b7fd2ae08f844204e72cf20ecf
tocode <andrey.zarev@outlook.com>
2024-04-03 13:58:19 +0300

    Update email-validator.ts

--- exam-project/src/app/shared/utils/email-validator.ts
+++ exam-project/src/app/shared/utils/email-validator.ts
@@ -24,7 +24,7 @@ export function validateEmail(domains: string[]): ValidatorFn {
     const domainString = domains.join('|');
     // const regExp = new RegExp(`\w*.?\w*@\w{2,}\.(com|bg)`)
     const regExp = new RegExp(`[A-Za-z0-9]+@[A-Za-z]+\.(${domainString})`);
-    debugger
+
     return (control) => {
         const isEmailInvalid = control.value === '' || regExp.test(control.value);
         return isEmailInvalid ? null : { validateEmail: true };
