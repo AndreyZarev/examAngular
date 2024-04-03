48b21fd272cdf8362da918af9edcc96b3201f54a
tocode <andrey.zarev@outlook.com>
2024-04-03 13:58:27 +0300

    Update register.component.ts

--- exam-project/src/app/views/register/register.component.ts
+++ exam-project/src/app/views/register/register.component.ts
@@ -12,7 +12,31 @@ export class RegisterComponent {
 
   constructor(private userService: UserService, private router: Router,) { }
 
+
+  clicked: boolean = true;
+  registerButton() {
+
+    this.clicked = !this.clicked
+    return this.clicked;
+  }
+
+
   register(form: NgForm): void {
+    console.log(form.value);
+    debugger
+    if (form.invalid) {
+      alert('Your form is invalid, please follow the text below the fields.')
+      return;
+    }
+
+    try {
+      this.userService.login()
+
+
+    } catch (err) {
+      console.log(err);
+    }
+    this.router.navigate(['/home'])
 
   }
 }
