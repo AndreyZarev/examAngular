0d8abb7fdbf7b7877c5a0fcc1202f13dad1c9f6f
tocode <andrey.zarev@outlook.com>
2024-04-03 21:34:37 +0300

    Update offers.component.ts

--- exam-project/src/app/views/offers/offers.component.ts
+++ exam-project/src/app/views/offers/offers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../service-api/service-api.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-offers',
   templateUrl: './offers.component.html',
@@ -8,7 +9,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class OffersComponent implements OnInit {
   catalog: any = {}
-  constructor(private apiService: ApiService, private acktivRoute: ActivatedRoute, private router: Router) { }
+  constructor(private apiService: ApiService, private acktivRoute: ActivatedRoute, private router: Router, fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.acktivRoute.params.subscribe((data) => {
@@ -18,22 +19,43 @@ export class OffersComponent implements OnInit {
       this.apiService.getCatalog().subscribe((data) => {
         console.log(data);
         this.catalog = data
+
       })
     })
   }
+  editable: boolean = false
   edit(id: string): void {
     debugger
+
 
     console.log(id);
 
     this.apiService.getOffer(id).subscribe((data) => {
+
+      debugger
       console.log(data);
       this.catalog = data
+      this.editable = true
     })
-    debugger
+
     this.router.navigate(['/offers/edit']);
 
   }
+
+  // saveProfileHandle(): void {
+  //   if (this.form.invalid) {
+  //     return;
+  //   }
+
+  //   this.profileDetails = this.form.value as ProfileDetails;
+  //   this.onToggle();
+  // }
+
+  // onCancel(e: Event) {
+  //   e.preventDefault();
+  //   this.onToggle();
+  // }
+
 
 
 }
