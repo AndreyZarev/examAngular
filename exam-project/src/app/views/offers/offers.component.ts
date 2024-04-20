40975bc14273fc647a1b03345209503f4a2eecde
tocode <andrey.zarev@outlook.com>
2024-04-20 17:30:55 +0300

    Update offers.component.ts

--- exam-project/src/app/views/offers/offers.component.ts
+++ exam-project/src/app/views/offers/offers.component.ts
@@ -28,7 +28,7 @@ export class OffersComponent implements OnInit {
     })
   }
 
-  form = this.fb.group({
+  furnitureDetails = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(2)]],
     email: ['',
       [Validators.required, validateEmail(["bg", "com"])]
