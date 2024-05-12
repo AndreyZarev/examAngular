d51f0c270cba50975bc91487de432d51fbb55915
tocode <andrey.zarev@outlook.com>
2024-05-12 18:32:02 +0300

    Update register.component.ts

--- exam-project/src/app/views/register/register.component.ts
+++ exam-project/src/app/views/register/register.component.ts
@@ -25,7 +25,7 @@ ngOnInit(): void {
     email: ['', [Validators.required,  ]],
   
    tel: ['', [Validators.required, Validators.minLength(7)]],
-     passGroup: this.fb.nonNullable(
+     passGroup: this.fb.group(
       {
         password: ['', [Validators.required]],
         rePassword: ['', [Validators.required]],
