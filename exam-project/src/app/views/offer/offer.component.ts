540308f0ceebad998be7b30d0d9f7560df3e12a9
tocode <andrey.zarev@outlook.com>
2024-09-08 23:15:32 +0300

    Update offer.component.ts

--- exam-project/src/app/views/offer/offer.component.ts
+++ exam-project/src/app/views/offer/offer.component.ts
@@ -7,7 +7,31 @@ import { Component } from '@angular/core';
 })
 export class OfferComponent {
 
+  ngOnInit(): void {
+    this.acktiveRoute.params.subscribe((data) => {
+      const id = data["id"]
+      console.log(this.myForm.value);
+      this.myForm = this.fb.group({
+        name: ['', [Validators.required, Validators.minLength(2)]],
+        email: ['',
+          [Validators.required, validateEmail(["bg", "com"])]
+        ],
+        tel: ['', [Validators.required, Validators.minLength(2)]],
+        password: ['', [Validators.required, Validators.minLength(2)]],
+        rePassword: ['', [Validators.required, Validators.minLength(2)]],
+      });
 
+      this.apiService.getCatalog().subscribe((data) => {
+        console.log("this is the catalog");
+
+        // this.catalog = data.slice()
+        console.log(data);
+
+
+      })
+    })
+  }
+  temesDetails: Theme[] | null = []
   edit(id: string): void {
 
 
