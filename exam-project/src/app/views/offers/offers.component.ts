d5c44ffad32e406a4a954f1cefce6abbb031c542
tocode <andrey.zarev@outlook.com>
2024-09-10 18:12:24 +0300

    Update offers.component.ts

--- exam-project/src/app/views/offers/offers.component.ts
+++ exam-project/src/app/views/offers/offers.component.ts
@@ -35,7 +35,7 @@ export class OffersComponent implements OnInit {
       this.apiService.getCatalog().subscribe((data) => {
         console.log("this is the catalog");
 
-        // this.catalog = data.slice()
+
         console.log(data);
 
 
