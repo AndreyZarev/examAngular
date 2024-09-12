201e451ad49bfc458c1cdc94ca5280ed857ef637
tocode <andrey.zarev@outlook.com>
2024-09-12 23:35:05 +0300

    Update login.component.ts

--- exam-project/src/app/views/login/login.component.ts
+++ exam-project/src/app/views/login/login.component.ts
@@ -2,14 +2,14 @@ import { Component } from '@angular/core';
 import { UserService } from 'src/app/user.service';
 import { Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
-
+import { EmailValidator } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private userService: UserService, private router: Router,) { }
+  constructor(private userService: UserService, private router: Router, emailValidator: EmailValidator) { }
 
   clicked: boolean = true;
   loginButton() {
