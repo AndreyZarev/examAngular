c689c3b7049a11feae777ada68a00fa2a8e39a3c
tocode <andrey.zarev@outlook.com>
2024-09-15 22:51:20 +0300

    Update views.module.ts

--- exam-project/src/app/views/views.module.ts
+++ exam-project/src/app/views/views.module.ts
@@ -3,13 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { RouterLink } from '@angular/router';
 import { NavbarModule } from '../navbar/navbar.module';
-// import { HomeComponent } from '../views/home/home.component'; '';
-// import { RegisterComponent } from '../views/register/register.component';
-// import { LoginComponent } from '../views/login/login.component';
-// import { LogoutComponent } from '../views/logout/logout.component';
-// import { CreateOfferComponent } from '../views/create-offer/create-offer.component';
-// import { OffersComponent } from '../views/offers/offers.component';
-// import { ErrorPageComponent } from '../views/error-page/error-page.component';
+import { HomeComponent } from '../views/home/home.component'; '';
+import { RegisterComponent } from '../views/register/register.component';
+import { LoginComponent } from '../views/login/login.component';
+import { LogoutComponent } from '../views/logout/logout.component';
+import { CreateOfferComponent } from '../views/create-offer/create-offer.component';
+import { OffersComponent } from '../views/offers/offers.component';
+import { ErrorPageComponent } from '../views/error-page/error-page.component';
 
 import { RouterModule, Routes } from '@angular/router';
 
