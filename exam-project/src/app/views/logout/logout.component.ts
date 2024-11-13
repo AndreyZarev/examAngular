5ed0a36a43f72ddf6394f8267d4dec8ee46e3761
tocode <andrey.zarev@outlook.com>
2024-11-13 19:01:57 +0200

    Update logout.component.ts

--- exam-project/src/app/views/logout/logout.component.ts
+++ exam-project/src/app/views/logout/logout.component.ts
@@ -1,18 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/user.service';
+import { OnInit } from '@angular/core';
+
+
 @Component({
   selector: 'app-logout',
   templateUrl: './logout.component.html',
   styleUrls: ['./logout.component.css']
 })
-export class LogoutComponent {
-  constructor(private userService: UserService) { }
-
-  logout() {
-
-    this.userService.logout
+export class LogoutComponent implements OnInit {
 
 
+
+
+
+  constructor(private userService: UserService, private router: Router) { }
+
+
+
+  ngOnInit() {
+
+    this.userService.logout()
+    this.router.navigate(['/home']);
 
   }
+
 }
