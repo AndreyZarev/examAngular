9453498a93d75fbf45218a7adc1f5291b00db3cc
tocode <andrey.zarev@outlook.com>
2024-11-14 22:46:28 +0200

    Update navbar.module.ts

--- exam-project/src/app/navbar/navbar.module.ts
+++ exam-project/src/app/navbar/navbar.module.ts
@@ -8,7 +8,7 @@ import { RegisterComponent } from '../views/register/register.component';
 import { LoginComponent } from '../views/login/login.component';
 import { LogoutComponent } from '../views/logout/logout.component';
 import { CreateOfferComponent } from '../views/create-offer/create-offer.component';
-import { OfferComponent } from '../views/offer/offer.component';
+import { OfferComponent } from '../views/offers/offers.component';
 import { ErrorPageComponent } from '../views/error-page/error-page.component';
 
 import { RouterModule, Routes } from '@angular/router';
