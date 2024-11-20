b399444847770eb253fe738d02c6070662f56eeb
tocode <andrey.zarev@outlook.com>
2024-11-20 02:17:43 +0200

    Update register.component.ts

--- exam-project/src/app/views/register/register.component.ts
+++ exam-project/src/app/views/register/register.component.ts
@@ -3,7 +3,7 @@ import { UserService } from 'src/app/user.service';
 import { Router } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, NgModel, NgForm } from '@angular/forms';
 import { validateEmail } from 'src/app/shared/utils/email-validator';
-import {passMatch} from 'src/app/shared/utils/pass-match'
+import { passMatch } from 'src/app/shared/utils/pass-match'
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -13,12 +13,12 @@ export class RegisterComponent {
   constructor(
     private userService: UserService,
     private router: Router,
-   
 
-  ) {}
-  
 
- 
+  ) { }
+
+
+
   register(form: NgForm): void {
 
     // console.log(form.value);
@@ -28,71 +28,32 @@ export class RegisterComponent {
       return;
     }
 
-    try {
-      const { email, password,rePassword } = form.value
-      debugger
-      this.userService.register(email, password).subscribe(() => {
-        // localStorage.setItem("user", email)
-        this.router.navigate(['/home']);
-      });
-
-
-    } catch (err) {
-      console.log(err);
+    const { email, password, rePassword } = form.value
+    debugger
+    this.userService.register(email, password).subscribe({
+      error: (e) => alert("You have already been registered!"),
+      complete: () => {
+        localStorage.setItem('user', email)
+        this.router.navigate(['/home'])
+      }
+      ,
     }
+    )
+
+
+
+
+
+
+
 
   }
 
 }
 
- 
 
 
 
- 
-
-//   register(): void {
-//     if (this.form.invalid) {
-//       return alert('Invalid form');
-//     }
-
-//     const {
-    
-//       email,
-     
-//        password, 
-//        rePassword ,
-//     } = this.form.value;
 
 
-//     this.userService
-//       .register( email!, password!)
-//       .subscribe(() => {
-//         this.router.navigate(['/home']);
-//       });
-//   }
-// }
 
-
-//   clicked: boolean = true;
-//   registerButton() {
-
-//     this.clicked = !this.clicked
-//     return this.clicked;
-//   }
-
-
-//   register(): void {
-   
-//     try {
-//       this.userService.register()
-//       this.userService.login()
-
-
-//     } catch (err) {
-//       console.log(err);
-//     }
-//     this.router.navigate(['/home'])
-
-//   }
-// }
