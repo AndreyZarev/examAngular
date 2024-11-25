0496c58c064e51ef398157620153620806bdd496
tocode <andrey.zarev@outlook.com>
2024-11-25 09:52:54 +0200

    Update offer-edit.component.ts

--- exam-project/src/app/views/offer-edit/offer-edit.component.ts
+++ exam-project/src/app/views/offer-edit/offer-edit.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
+import { ApiService } from '../service-api/service-api.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-offer-edit',
   templateUrl: './offer-edit.component.html',
   styleUrls: ['./offer-edit.component.css']
 })
 export class OfferEditComponent {
+  constructor(private apiService: ApiService, private router: Router) { }
 
+  updateOffer(form: NgForm) {
+
+    // ev.preventDefault();
+    if (form.invalid) {
+      alert('Your form is invalid, please follow the text below the fields.')
+      return;
+    }
+    try {
+
+      let { img, names, email, hand, weight, place, bet } = form.value
+      debugger
+      this.apiService.newOffer(img, names, email, hand, weight, place, bet).subscribe({
+        error: (e) => console.error(e),
+        complete: () => console.info('complete')
+      }
+      )
+
+      this.router.navigate(['/offers'])
+
+
+    } catch (err) {
+      console.log(err);
+
+    }
+
+  }
 }
