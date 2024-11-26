11ab6847dc93fdcf5fe91898ab788654219aa246
tocode <andrey.zarev@outlook.com>
2024-11-26 15:17:34 +0200

    Update offer-edit.component.ts

--- exam-project/src/app/views/offer-edit/offer-edit.component.ts
+++ exam-project/src/app/views/offer-edit/offer-edit.component.ts
@@ -1,21 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ApiService } from '../service-api/service-api.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Offer } from 'src/interface/offer';
 @Component({
   selector: 'app-offer-edit',
   templateUrl: './offer-edit.component.html',
   styleUrls: ['./offer-edit.component.css']
 })
-export class OfferEditComponent
-// implements OnInit 
-{
-  constructor(private apiService: ApiService, private router: Router) { }
+export class OfferEditComponent implements OnInit {
+  constructor(private apiService: ApiService, private router: Router, private route: ActivatedRoute) { }
+  offer = {} as Offer;
 
-
+  offerId!: string;
+  optionsHand = [
+    { value: 'Right', label: 'Option 1' },
+    { value: 'Left', label: 'Option 2' },
+    { value: 'Both', label: 'Option 3' }
+  ];
   ngOnInit() {
-    // this.apiService.getOffer(id)
+    this.offerId = this.route.snapshot.paramMap.get('id') || '';
+
+
+
+    this.route.paramMap.subscribe((params) => {
+      this.offerId = params.get('id') || '';
+
+      this.apiService.getOffer(this.offerId).subscribe((data) => {
+        console.log('Offer data:', data);
+        this.offer = data
+      })
+
+    })
   }
+
   updateOffer(form: NgForm) {
 
     // ev.preventDefault();
