281afc2c3f5f91ee85ca5cfc88f778c8b2deb026
tocode <andrey.zarev@outlook.com>
2024-11-30 18:11:41 +0200

    Update offer-edit.component.ts

--- exam-project/src/app/views/offer-edit/offer-edit.component.ts
+++ exam-project/src/app/views/offer-edit/offer-edit.component.ts
@@ -48,7 +48,7 @@ export class OfferEditComponent implements OnInit {
 
 
         debugger
-        this.apiService.updateOffer(img, names, email, hand, weight, place, bet, form.value._id).subscribe({
+        this.apiService.updateOffer(img, names, email, hand, weight, place, bet, this.offerId).subscribe({
           error: (e) => console.error(e),
           complete: () => console.info('complete')
         }
