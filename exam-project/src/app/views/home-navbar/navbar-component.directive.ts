1de393e9c3f80368e893d4640d5233a85b67badc
tocode <andrey.zarev@outlook.com>
2024-12-09 12:34:16 +0200

    Create navbar-component.directive.ts

---
+++ exam-project/src/app/views/home-navbar/navbar-component.directive.ts
@@ -0,0 +1,61 @@
+import { AfterViewInit, Directive, ElementRef, OnChanges, OnInit, Renderer2, SimpleChanges } from '@angular/core';
+import { UserService } from '../../service-api/user.service';
+@Directive({
+  selector: '[appNavbarComponent]'
+})
+export class NavbarComponentDirective implements OnInit {
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2, private userService: UserService) { }
+
+  get isLoggedIn(): boolean {
+
+
+
+    return this.userService.isLogged
+
+
+  }
+  ngOnInit(): void {
+
+    if (this.isLoggedIn == true && localStorage.getItem('user') !== undefined) {
+      this.renderer.addClass(this.elRef.nativeElement, "navAncors")
+      this.renderer.removeClass(this.elRef.nativeElement, "navAncors2")
+
+    } else {
+      this.renderer.removeClass(this.elRef.nativeElement, "navAncors")
+      this.renderer.addClass(this.elRef.nativeElement, "navAncors2")
+    }
+
+
+
+
+  }
+  classPick = "navAncors"
+  checkLogin() {
+    if (this.isLoggedIn) {
+      this.classPick = "navAncors"
+    }
+    if (this.isLoggedIn) {
+      this.classPick = "nav2Ancors"
+    }
+
+  }
+
+
+
+
+
+
+
+  mouseHoverEventHandler() {
+
+
+
+
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', "pink")
+  }
+
+  mouseOutEventHandler() {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', "initial")
+  }
+}
