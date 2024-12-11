3fbc3363d98e19b3313c08d01e4d06aa4bb55141
tocode <andrey.zarev@outlook.com>
2024-12-12 00:58:33 +0200

    Create create-offer.component.ts

---
+++ exam-project/src/app/views/offers-edit-create/create-offer/create-offer.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../../../service-api/service-api.service';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-offer',
+  templateUrl: './create-offer.component.html',
+  styleUrls: ['./create-offer.component.css']
+})
+export class CreateOfferComponent {
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  email: string | null = localStorage.getItem('email') || null;
+  createOffer(form: NgForm) {
+
+    // ev.preventDefault();
+    if (form.invalid) {
+      alert('Your form is invalid, please follow the text below the fields.')
+      return;
+    }
+    try {
+
+      let { img, names, email, hand, weight, place, bet } = form.value
+      debugger
+      this.apiService.newOffer(img, names, email, hand, weight, place, bet).subscribe({
+        error: (e) => console.error(e),
+        complete: () => console.info('complete')
+      }
+      )
+
+      this.router.navigate(['/offers'])
+
+
+    } catch (err) {
+      console.log(err);
+
+    }
+
+  }
+}
