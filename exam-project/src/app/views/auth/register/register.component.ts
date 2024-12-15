fd13959f0b99502ab7fd56f2df92d89eba967926
tocode <andrey.zarev@outlook.com>
2024-12-15 13:49:03 +0200

    removed not used imports

--- exam-project/src/app/views/auth/register/register.component.ts
+++ exam-project/src/app/views/auth/register/register.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../../service-api/user.service';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators, NgModel, NgForm } from '@angular/forms';
-import { validateEmail } from 'src/app/shared/utils/email-validator';
-import { passMatch } from 'src/app/shared/utils/pass-match'
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
